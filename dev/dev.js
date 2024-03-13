dca980d34a43c9a3e011a1b8f4ab08ad2efd20ab
Benjamin Degenhart <b.degenhart.work@gmail.com>
2024-03-13 22:44:19 +0100

    Dev script to read in files from db

--- dev/dev.js
+++ dev/dev.js
@@ -1,3 +1,37 @@
-import { func } from "../src/index.js"
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
 
-func()
+const DB_DIR = path.join(path.dirname(fileURLToPath(import.meta.url)), "db")
+const SHACL_DIR = `${DB_DIR}/shacl`
+const USER_PROFILE = `${DB_DIR}/user-profile.ttl`
+
+fs.readdir(SHACL_DIR, async (err, files) => {
+    if (err) { console.error(err); return }
+
+    let userProfile = ""
+    let requirementProfiles = []
+
+    let promises = files.map(shaclFile => {
+        return new Promise((resolve, reject) => {
+            fs.readFile(`${SHACL_DIR}/${shaclFile}`, "utf8", (err, data) => {
+                if (err) { reject(err); return }
+                requirementProfiles.push(data)
+                resolve()
+            })
+        })
+    })
+    promises.push(new Promise((resolve, reject) => {
+        fs.readFile(USER_PROFILE, "utf8", (err, data) => {
+            if (err) { reject(err); return }
+            userProfile = data
+            resolve()
+        })
+    }))
+
+    Promise.all(promises)
+        .then(() => {
+            console.log(requirementProfiles)
+            console.log(userProfile)
+        }).catch(err => console.error(err))
+})
