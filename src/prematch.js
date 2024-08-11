50fabf536df1d23eaf76ea55084837e85b767f17
Benjamin Degenhart <b.degenhart.work@gmail.com>
2024-08-11 13:11:04 +0200

    Minor reformatting of getBenefitCategories()

--- src/prematch.js
+++ src/prematch.js
@@ -1,11 +1,11 @@
 import { rdfStringsToStore, runSparqlSelectQueryOnStore } from "./utils.js"
 
-function uri(uri, shorten) {
+function shortenUri(uri, shorten) {
     return shorten ? "ff:" + uri.split("#")[1] : uri
 }
 
-export async function getBenefitCategories(datafieldsStr, reqProfilesStr, shortenUri = false) {
-    let store = await rdfStringsToStore([datafieldsStr, ...reqProfilesStr])
+export async function getBenefitCategories(datafieldsStr, reqProfilesStrArr, doShortenUri = false) {
+    let store = await rdfStringsToStore([datafieldsStr, ...reqProfilesStrArr])
     let categories = {}
     let requirementProfiles = {}
     let query = `
@@ -19,7 +19,7 @@ export async function getBenefitCategories(datafieldsStr, reqProfilesStr, shorte
         }`
     let rows = await runSparqlSelectQueryOnStore(query, store)
     for (let row of rows) {
-        categories[uri(row.category, shortenUri)] = {
+        categories[shortenUri(row.category, doShortenUri)] = {
             de: row.de,
             en: row.en,
             requirementProfiles: []
@@ -28,27 +28,26 @@ export async function getBenefitCategories(datafieldsStr, reqProfilesStr, shorte
     query = `
         PREFIX ff: <https://foerderfunke.org/default#>
         SELECT * WHERE {
-            ?rq a ff:RequirementProfile .
-            ?rq ff:title ?de .
+            ?rp a ff:RequirementProfile .
+            ?rp ff:title ?de .
             FILTER (lang(?de) = "de")
-            ?rq ff:title ?en .
+            ?rp ff:title ?en .
             FILTER (lang(?en) = "en")
-            ?rq ff:category ?category .               
+            ?rp ff:category ?category .               
         }`
     rows = await runSparqlSelectQueryOnStore(query, store)
-    console.log(rows)
     for (let row of rows) {
-        let rqUri = uri(row.rq, shortenUri)
-        let categoryUri = uri(row.category, shortenUri)
-        categories[categoryUri].requirementProfiles.push(rqUri)
-        if (!requirementProfiles[rqUri]) {
-            requirementProfiles[rqUri] = {
+        let rpUri = shortenUri(row.rp, doShortenUri)
+        let categoryUri = shortenUri(row.category, doShortenUri)
+        categories[categoryUri].requirementProfiles.push(rpUri)
+        if (!requirementProfiles[rpUri]) {
+            requirementProfiles[rpUri] = {
                 de: row.de,
                 en: row.en,
                 categories: []
             }
         }
-        requirementProfiles[rqUri].categories.push(categoryUri)
+        requirementProfiles[rpUri].categories.push(categoryUri)
     }
     return {
         "byCategories": categories,
