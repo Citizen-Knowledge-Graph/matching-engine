b85525f1d4a288668c2a69313af453563989bf84
Benjamin Degenhart <b.degenhart.work@gmail.com>
2024-08-12 23:11:48 +0200

    Complete implementation of getPrioritizedMissingDataFieldsJson

--- src/prematch.js
+++ src/prematch.js
@@ -1,7 +1,7 @@
 import { convertReqProfilesStrArrToMap, rdfStringsToStore, runSparqlSelectQueryOnStore } from "./utils.js"
 import { validateAll } from "./index.js";
 
-function shortenUri(uri, shorten) {
+function shortenUri(uri, shorten = true) {
     return shorten ? "ff:" + uri.split("#")[1] : uri
 }
 
@@ -92,9 +92,69 @@ export async function getPrioritizedMissingDataFieldsJson(selectedBenefitCategor
 
     let report = await validateAll(userProfileStr, rpMapInFocus, datafieldsStr, materializationStr)
     let sorted = Object.entries(report.missingUserInputsAggregated).sort((a, b) => b[1].usedIn.length - a[1].usedIn.length)
+    let sortedUris = sorted.map(df => df[1].dfUri)
+    let sortedUrisShortened = sorted.map(df => shortenUri(df[1].dfUri))
 
-    console.log(sorted)
-    // TODO
+    let fieldsMap = {}
 
-    return []
+    // Query 1: get info about data fields and their datatype
+    let query = `
+        PREFIX ff: <https://foerderfunke.org/default#>
+        PREFIX sh: <http://www.w3.org/ns/shacl#>
+        SELECT * WHERE {
+            ?df a ff:DataField .
+            # ff:pensionable will also show up here but is not in datafields.ttl
+            VALUES ?df { ${sortedUrisShortened.join(" ")} }
+            ?df rdfs:label ?titleDe .
+            ?df rdfs:label ?titleEn .
+            ?df schema:question ?questionDe .
+            ?df schema:question ?questionEn .
+            FILTER (lang(?titleDe) = "de")
+            FILTER (lang(?titleEn) = "en")
+            FILTER (lang(?questionDe) = "de")
+            FILTER (lang(?questionEn) = "en")
+            ?df ff:objectConstraints ?constraints .
+            OPTIONAL { ?constraints sh:datatype ?datatype . }
+        }`
+    let rows = await runSparqlSelectQueryOnStore(query, store)
+    for (let row of rows) {
+        fieldsMap[row.df] = {
+            "datafield": shortenUri(row.df),
+            "titleDe": row.titleDe,
+            "titleEn": row.titleEn,
+            "questionDe": row.questionDe,
+            "questionEn": row.questionEn,
+            "datatype": row.datatype ? row.datatype.split("#")[1] : "selection",
+        }
+        if (!row.datatype) fieldsMap[row.df].choices = []
+    }
+
+    // Query 2: get choices for datafields that have selection as datatype
+    query = `
+        PREFIX ff: <https://foerderfunke.org/default#>
+        PREFIX sh: <http://www.w3.org/ns/shacl#>
+        SELECT * WHERE {
+            ?df a ff:DataField .
+            VALUES ?df { ${sortedUrisShortened.join(" ")} }
+            ?df ff:objectConstraints ?constraints .
+            ?constraints sh:in/rdf:rest*/rdf:first ?option .
+            ?option rdfs:label ?labelDe .
+            ?option rdfs:label ?labelEn .
+            FILTER (lang(?labelDe) = "de")
+            FILTER (lang(?labelEn) = "en")
+        }`
+    rows = await runSparqlSelectQueryOnStore(query, store)
+    for (let row of rows) {
+        fieldsMap[row.df].choices.push({
+            "value": shortenUri(row.option),
+            "labelDe": row.labelDe,
+            "labelEn": row.labelEn
+        })
+    }
+
+    return {
+        "id": "quick-check-profile",
+        "title": "About you",
+        "fields": sortedUris.map(uri => fieldsMap[uri])
+    }
 }
