0ac5c0c608070286c16ab622ab55aa0b711bed90
Benjamin Degenhart <b.degenhart.work@gmail.com>
2025-01-25 22:06:53 +0100

    Call it bundling instead of global

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,7 +1,7 @@
 const path = require("path")
 
 module.exports = {
-    entry: "./global.js",
+    entry: "./bundling.js",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
