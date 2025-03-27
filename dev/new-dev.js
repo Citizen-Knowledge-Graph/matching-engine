29dcea7bf36b26500bea2923e9d0ea31c4b44898
Benjamin Degenhart <b.degenhart.work@gmail.com>
2025-03-27 18:45:22 +0100

    Use sem-ops-utils in refactoring

--- dev/new-dev.js
+++ dev/new-dev.js
@@ -1,44 +1,22 @@
 import path from "path"
 import { fileURLToPath } from "url"
 import { promises } from "fs"
-import { buildValidator, datasetToTurtle, runValidation, storeFromTurtle, extractRpUriFromRpStr, buildValidators, turtleToDataset } from "../src/new/basics.js"
-import { runMatching } from "../src/new/main.js"
+import { buildInMemoryObject } from "../src/new/main.js"
 
-const ROOT = path.join(path.dirname(fileURLToPath(import.meta.url)), "requirement-profiles", "sozialplattform")
-const RPs = `${ROOT}/shacl`
-const UP = `${ROOT}/user-profile-dev.ttl`
-const DFs = `${ROOT}/datafields.ttl`
-const MATs = `${ROOT}/materialization.ttl`
+let inMemoryObject = {}
 
-async function testValidator() {
-    let up = await promises.readFile(UP, "utf8")
-    let rp = await promises.readFile(`${RPs}/03-kinderzuschlag.ttl`, "utf8")
-    const report = await runValidation(buildValidator(rp), turtleToDataset(up))
-    console.log(await datasetToTurtle(report.dataset))
-}
-
-async function buildValidatorsMap(includeUp = false) {
-    let map = {}
-    if (includeUp) map["up"] = await promises.readFile(UP, "utf8")
-    for (let file of await promises.readdir(RPs)) {
-        let rpStr = await promises.readFile(`${RPs}/${file}`, "utf8")
-        let rpUri = extractRpUriFromRpStr(rpStr)
-        if (rpUri) map[rpUri] = rpStr
+async function devBuildInMemoryObject() {
+    const ROOT = path.join(path.dirname(fileURLToPath(import.meta.url)), "requirement-profiles")
+    const rps = []
+    for (let file of await promises.readdir(`${ROOT}/shacl`)) {
+        rps.push(await promises.readFile(`${ROOT}/shacl/${file}`, "utf8"))
     }
-    return buildValidators(map)
+    inMemoryObject = buildInMemoryObject(
+        await promises.readFile(`${ROOT}/datafields.ttl`, "utf8"),
+        await promises.readFile(`${ROOT}/materialization.ttl`, "utf8"),
+        rps
+    )
+    console.log("inMemoryObject", Object.keys(inMemoryObject))
 }
 
-async function testValidators() {
-    console.log(await buildValidatorsMap(true))
-}
-
-async function testRunMatching() {
-    let up = await promises.readFile(UP, "utf8")
-    let profileStore = storeFromTurtle(up)
-    let map = await buildValidatorsMap()
-    await runMatching(profileStore, map)
-}
-
-// await testValidator()
-// await testValidators()
-await testRunMatching()
+await devBuildInMemoryObject()
--- src/new/main.js
+++ src/new/main.js
@@ -1,12 +1,13 @@
-import { runValidation, storeToDataset } from "./basics.js"
+import { buildValidator, extractFirstIndividualUriFromTurtle, storeFromTurtles } from "sem-ops-utils"
 
-export async function runMatching(profileStore, validatorsMap) {
-    // materialization TODO
-
-    const profileDataset = storeToDataset(profileStore)
-
-    for (let [rp, validator] of Object.entries(validatorsMap)) {
-        const report = await runValidation(validator, profileDataset)
-        console.log(rp, report.conforms)
+export function buildInMemoryObject(datafieldsTurtle, materializationTurtle, requirementProfilesTurtles) {
+    let obj = {}
+    obj.dfMatStore = storeFromTurtles([datafieldsTurtle, materializationTurtle])
+    obj.rpStore = storeFromTurtles(requirementProfilesTurtles)
+    obj.validators = {}
+    for (let rpStr of requirementProfilesTurtles) {
+        let rpUri = extractFirstIndividualUriFromTurtle(rpStr, "ff:RequirementProfile")
+        if (rpUri) obj.validators[rpUri] = buildValidator(rpStr)
     }
+    return obj
 }
