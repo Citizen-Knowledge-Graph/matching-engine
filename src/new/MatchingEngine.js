c846a8c061d471c47afbfc7580b2e4fde4772baa
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-03-29 11:02:50 +0100

    Restructure to allow adding validators later on

--- src/new/MatchingEngine.js
+++ src/new/MatchingEngine.js
@@ -1,27 +1,18 @@
-import { buildValidator, extractFirstIndividualUriFromTurtle, storeFromTurtles, turtleToDataset } from "sem-ops-utils"
+import { buildValidator, extractFirstIndividualUriFromTurtle, storeFromTurtles, turtleToDataset, newStore, addTurtleToStore } from "sem-ops-utils"
 
 export class MatchingEngine {
 
     constructor(datafieldsTurtle, materializationTurtle, requirementProfilesTurtles) {
-        this.buildKnowledgeBaseStore(datafieldsTurtle, materializationTurtle)
-        this.buildRequirementProfileStore(requirementProfilesTurtles)
-        this.buildValidators(requirementProfilesTurtles)
-    }
-
-    buildKnowledgeBaseStore(df, mat) {
-        this.knowledgeBaseStore = storeFromTurtles([df, mat])
-    }
-
-    buildRequirementProfileStore(rps) {
-        this.requirementProfilesStore = storeFromTurtles(rps)
-    }
-
-    buildValidators(rps) {
+        this.knowledgeBaseStore = storeFromTurtles([datafieldsTurtle, materializationTurtle])
+        this.requirementProfilesStore = newStore()
         this.validators = {}
-        for (let rpStr of rps) {
-            let rpUri = extractFirstIndividualUriFromTurtle(rpStr, "ff:RequirementProfile")
-            if (rpUri) this.validators[rpUri] = buildValidator(rpStr)
-        }
+        for (let rp of requirementProfilesTurtles) this.addValidator(rp)
+    }
+
+    addValidator(rp) {
+        addTurtleToStore(this.requirementProfilesStore, rp)
+        let rpUri = extractFirstIndividualUriFromTurtle(rp, "ff:RequirementProfile")
+        if (rpUri) this.validators[rpUri] = buildValidator(rp)
     }
 
     async validateOne(up, rp) {
