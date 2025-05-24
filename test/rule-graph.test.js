d7bcadfeb0d77bdb81d5831bd808fe7ce64dcd0c
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-05-24 09:08:47 +0200

    Add more element types to test case

--- test/rule-graph.test.js
+++ test/rule-graph.test.js
@@ -17,8 +17,8 @@ describe("rule graph", function () {
             ff:devShape a sh:NodeShape ;
             sh:targetClass ff:Citizen ;
             
-            sh:property [ sh:path ff:foo ; sh:in (true) ] ;
-            sh:property [ sh:path ff:bar ; sh:in (ff:blau) ] ;
+            sh:property [ sh:not [ sh:path ff:foo ; sh:minInclusive 15 ; sh:maxExclusive 36 ] ] ;
+            sh:property [ sh:path ff:bar ; sh:in (ff:blau ff:red) ] ;
             sh:property [
                 sh:or (
                     [ sh:and (
