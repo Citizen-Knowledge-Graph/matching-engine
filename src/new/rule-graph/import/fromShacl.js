4b601af28aa51f12ee4dc76918dc5629344849f6
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-05-24 20:49:56 +0200

    Refactor: treating top-level like every node

--- src/new/rule-graph/import/fromShacl.js
+++ src/new/rule-graph/import/fromShacl.js
@@ -1,50 +1,49 @@
 import { Node } from "../Graph.js"
 
-export function ruleGraphFromShacl(shape) {
-    const props = shape["sh:property"] ?? []
-    return new Node("ROOT", [new Node("AND", asArray(props).map(walk))])
-}
+export const ruleGraphFromShacl = shape => new Node("ROOT", [walk(shape)])
 
 function walk(obj, path = null) {
     path = obj["sh:path"]?.["@id"] ?? path
     const children = []
+    let notIn = null
     if (obj["sh:property"]) {
         children.push(... asArray(obj["sh:property"]).map(p => walk(p)))
     }
     if (obj["sh:not"]) {
         const inner = obj["sh:not"]
-        children.push(
-            canInlineNotIn(inner)
-                ? makeRule(inner, path, true)
-                : new Node("NOT", [walk(inner, path)])
-        )
-    }
-    for (const op of ["sh:or", "sh:and"]) {
-        if (obj[op]) {
-            children.push(new Node(op.slice(3).toUpperCase(), list(obj[op]).map(n => walk(n, path))))
+        if (canInlineNotIn(inner)) {
+            notIn = list(inner["sh:in"]).map(atom)
+        } else {
+            children.push(new Node("NOT", [walk(inner, path)]))
         }
     }
-    if (hasFacet(obj)) {
-        const rule = makeRule(obj, path)
+    for (const [key, label] of [["sh:or", "OR"], ["sh:and", "AND"]]) {
+        if (obj[key])
+            children.push(new Node(label, list(obj[key]).map(n => walk(n, path))))
+    }
+    if (hasFacet(obj) || notIn) {
+        const rule = makeRule(obj, path, notIn)
         if (!children.length) return rule
         children.unshift(rule)
     }
-    return children.length > 1 ? new Node("AND", children) : children[0]
+    if (children.length === 1) return children[0]
+    if (children.length >  1) return new Node("AND", children)
+    throw new Error("Unhandled fragment:\n" + JSON.stringify(obj, null, 2))
 }
 
-function makeRule(p, path, neg = false) {
+function makeRule(p, inheritedPath, notIn = null) {
     const rule = Object.fromEntries(
         [
-            ["path", p["sh:path"]?.["@id"] ?? path],
+            ["path", p["sh:path"]?.["@id"] ?? inheritedPath],
             ["minCount", num(p["sh:minCount"])],
             ["maxCount", num(p["sh:maxCount"])],
             ["minInclusive", num(p["sh:minInclusive"])],
             ["minExclusive", num(p["sh:minExclusive"])],
             ["maxInclusive", num(p["sh:maxInclusive"])],
             ["maxExclusive", num(p["sh:maxExclusive"])],
-            ["in", !neg && p["sh:in"] ? list(p["sh:in"]).map(atom) : null],
-            ["notIn", neg && p["sh:in"] ? list(p["sh:in"]).map(atom) : null]
-        ].filter(([, v]) => v !== null)
+            ["in", p["sh:in"] ? list(p["sh:in"]).map(atom) : null],
+            ["notIn", notIn]
+        ].filter(([, v]) => v != null)
     )
     if (!rule.path) throw new Error("No path for rule:\n" + JSON.stringify(p))
     return new Node("RULE", [], rule)
