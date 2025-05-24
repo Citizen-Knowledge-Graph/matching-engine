a2700e6b3004a0896b4195e88e17e38e7b89b110
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-05-24 18:42:52 +0200

    Reorganize with new rule-graph folder

--- src/new/MatchingEngine.js
+++ src/new/MatchingEngine.js
@@ -1,9 +1,9 @@
 import { buildValidator, extractFirstIndividualUriFromTurtle, storeFromTurtles, turtleToDataset, newStore, addTurtleToStore, storeFromDataset, sparqlConstruct, storeToTurtle, sparqlSelect, addTriple, expand, a, datasetFromStore, storeToJsonLdObj, sparqlInsertDelete, formatTimestamp, formatTimestampAsLiteral, addStoreToStore } from "@foerderfunke/sem-ops-utils"
 import { FORMAT, MATCHING_MODE, QUERY_ELIGIBILITY_STATUS, QUERY_MISSING_DATAFIELDS, QUERY_NUMBER_OF_MISSING_DATAFIELDS, QUERY_TOP_MISSING_DATAFIELD, QUERY_BUILD_INDIVIDUALS_TREE, QUERY_EXTRACT_INVALID_INDIVIDUALS, QUERY_HASVALUE_FIX, QUERY_METADATA_RPS, QUERY_METADATA_DFS, QUERY_METADATA_BCS, QUERY_INSERT_VALIDATION_REPORT_URI, QUERY_DELETE_NON_VIOLATING_VALIDATION_RESULTS, QUERY_LINK_REPORT_ONLY_IF_EXISTS } from "./queries.js"
-import { Graph } from "./Graph.js"
+import { Graph } from "./rule-graph/Graph.js"
 import { inspect } from "util"
-import { ruleGraphFromShacl } from "./import/fromShacl.js"
-import { ruleGraphToMermaid } from "./export/toMermaid.js"
+import { ruleGraphFromShacl } from "./rule-graph/import/fromShacl.js"
+import { ruleGraphToMermaid } from "./rule-graph/export/toMermaid.js"
 
 export class MatchingEngine {
 
