cf2ae943470edd2976f67d7a302848df3ff747c8
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-05-31 15:39:17 +0200

    Change rule graph struct: leaves below rule node

--- src/new/rule-graph/import/fromShacl.js
+++ src/new/rule-graph/import/fromShacl.js
@@ -6,51 +6,57 @@ function walk(obj, path = null) {
     path = obj["sh:path"]?.["@id"] ?? path
     const children = []
     if (obj["sh:property"]) {
-        children.push(... asArray(obj["sh:property"]).map(p => walk(p)))
+        children.push(... arr(obj["sh:property"]).map(p => walk(p)))
     }
     if (obj["sh:not"]) {
         const inner = obj["sh:not"]
-        children.push(new Node("NOT", [walk(inner, path)]))
+        if (canInlineNotIn(inner)) {
+            children.push(new Node("NOT", [constraintNode("IN", list(inner["sh:in"]).map(atom))]))
+        } else {
+            children.push(new Node("NOT", [walk(inner, path)]))
+        }
     }
-    for (const [key, label] of [["sh:or", "OR"], ["sh:and", "AND"]]) {
-        if (obj[key]) children.push(new Node(label, list(obj[key]).map(n => walk(n, path))))
+    for (const [k, lbl] of [["sh:or", "OR"], ["sh:and", "AND"]]) {
+        if (obj[k]) children.push(new Node(lbl, list(obj[k]).map(n => walk(n, path))))
     }
-    if (hasFacet(obj)) {
-        const rule = makeRule(obj, path)
-        if (!children.length) return rule
-        children.unshift(rule)
+    const facetLeaves = buildFacetNodes(obj)
+    if (facetLeaves.length) {
+        const pathNode = new Node("RULE",
+            facetLeaves.length === 1 ? facetLeaves
+                : [new Node("AND", facetLeaves)]
+        )
+        pathNode.path = path
+        if (!children.length) return pathNode
+        children.unshift(pathNode)
     }
     if (children.length === 1) return children[0]
     if (children.length >  1) return new Node("AND", children)
     throw new Error("Unhandled fragment:\n" + JSON.stringify(obj, null, 2))
 }
 
-function makeRule(p, inheritedPath) {
-    const rule = Object.fromEntries(
-        [
-            ["path", p["sh:path"]?.["@id"] ?? inheritedPath],
-            ["minCount", num(p["sh:minCount"])],
-            ["maxCount", num(p["sh:maxCount"])],
-            ["minInclusive", num(p["sh:minInclusive"])],
-            ["minExclusive", num(p["sh:minExclusive"])],
-            ["maxInclusive", num(p["sh:maxInclusive"])],
-            ["maxExclusive", num(p["sh:maxExclusive"])],
-            ["in", p["sh:in"] ? list(p["sh:in"]).map(atom) : null],
-            ["hasValue", p["sh:hasValue"] ? atom(p["sh:hasValue"]) : null],
-            // TODO add more
-        ].filter(([, v]) => v != null)
-    )
-    if (!rule.path) throw new Error("No path for rule:\n" + JSON.stringify(p))
-    return new Node("RULE", [], rule)
+function buildFacetNodes(o) {
+    const leaves = []
+    if (o["sh:in"])           leaves.push(constraintNode("IN", list(o["sh:in"]).map(atom)))
+    if (o["sh:hasValue"])     leaves.push(constraintNode("HAS_VALUE", atom(o["sh:hasValue"])))
+    if (o["sh:minCount"])     leaves.push(constraintNode("MIN_COUNT", num(o["sh:minCount"])))
+    if (o["sh:maxCount"])     leaves.push(constraintNode("MAX_COUNT", num(o["sh:maxCount"])))
+    if (o["sh:minInclusive"]) leaves.push(constraintNode("MIN_INC",  num(o["sh:minInclusive"])))
+    if (o["sh:minExclusive"]) leaves.push(constraintNode("MIN_EXC",  num(o["sh:minExclusive"])))
+    if (o["sh:maxInclusive"]) leaves.push(constraintNode("MAX_INC",  num(o["sh:maxInclusive"])))
+    if (o["sh:maxExclusive"]) leaves.push(constraintNode("MAX_EXC",  num(o["sh:maxExclusive"])))
+    return leaves
 }
 
-const FACET_KEYS = new Set(["sh:hasValue", "sh:in", "sh:minInclusive", "sh:minExclusive", "sh:maxInclusive", "sh:maxExclusive", "sh:minCount", "sh:maxCount"])
+function constraintNode(kind, value) {
+    const n = new Node(kind, [])
+    n.value = value
+    return n
+}
 
-const hasFacet = o => [... FACET_KEYS].some(k => k in o)
-
+const canInlineNotIn = o => Object.keys(o).every(k => k === "sh:path" || k === "sh:in")
 const list = x => x?.["@list"] ?? []
-const num = lit => lit ? Number(lit["@value"]) : null
-const asArray = x => (Array.isArray(x) ? x : [x])
+const num  = lit => lit ? Number(lit["@value"]) : null
+const arr  = x => (Array.isArray(x) ? x : [x])
 
 function atom(lit) {
     if (lit["@id"]) return lit["@id"]
