fb5463cce8c00614b4f1548071900f8d64d75342
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-06-11 17:24:01 +0200

    Add test leading from df to individuals class

--- test/multiple-individuals.test.js
+++ test/multiple-individuals.test.js
@@ -1,7 +1,7 @@
 import "./fixtures/common.js"
 import { describe } from "mocha"
 import { strictEqual } from "node:assert"
-import { expand, sparqlAsk } from "@foerderfunke/sem-ops-utils"
+import { expand, sparqlAsk, sparqlSelect } from "@foerderfunke/sem-ops-utils"
 import { FORMAT, MATCHING_MODE } from "../src/new/queries.js"
 
 describe("multiple individuals tests", function () {
@@ -29,6 +29,26 @@ describe("multiple individuals tests", function () {
                 ] .`
         matchingEngine.addValidator(shacl1)
         await matchingEngine.init()
+    })
+
+    it("should lead to class of children individuals", async function () {
+        const up = `
+            @prefix ff: <https://foerderfunke.org/default#> .        
+            ff:mainPerson a ff:Citizen .`
+        let reportStore = await matchingEngine.matching(up, [expand("ff:shacl1")], MATCHING_MODE.QUIZ, FORMAT.STORE, true)
+        let query = `
+            PREFIX ff: <https://foerderfunke.org/default#>
+            PREFIX sh: <http://www.w3.org/ns/shacl#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            SELECT * WHERE {
+                ?report ff:hasMostMissedDatafield ?dfIdentifier .
+                ?dfIdentifier rdf:predicate ?df .
+                ?df ff:hasShaclShape ?shape .
+                ?shape sh:property ?property .
+                OPTIONAL { ?property sh:class ?class }            
+            }`
+        let rows = await sparqlSelect(query, [reportStore, matchingEngine.dfMatStore])
+        strictEqual(rows[0].class, expand("ff:Child"), "ff:hasChild as missing datafield should lead to ff:Child class")
     })
 
     it("missing datafields in simple multi-individual case", async function () {
