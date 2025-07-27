3908a2a0dc329f2f2ee686d6f1e2d37c141a7a7e
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-07-27 22:26:44 +0200

    Add basic turtle functions to Profile class

--- src/ProfileManager.js
+++ src/ProfileManager.js
@@ -1,8 +1,13 @@
-import { newStore } from "@foerderfunke/sem-ops-utils"
+import { expand, newStore, nnExpand, parseObject, storeToTurtle } from "@foerderfunke/sem-ops-utils"
 
 export class ProfileManager {
     constructor() {
         this.profiles = {}
+    }
+    newProfile() {
+        let id = "profile" + (Object.keys(this.profiles).length + 1)
+        this.profiles[id] = new Profile(id)
+        return id
     }
     import() {}
     exportAll() {}
@@ -16,12 +21,27 @@ class Profile {
         this.nickname = null
         this.store = newStore()
     }
-    addEntry(individual, datafield, value) {}
-    changeEntry(individual, datafield, newValue) {}
-    removeEntry(individual, datafield) {}
-    addIndividual(clazz) {}
+    addEntry(individual, datafield, value) {
+        this.store.addQuad(nnExpand(individual), nnExpand(datafield), parseObject(value))
+    }
+    changeEntry(individual, datafield, oldValue, newValue) {
+        if (oldValue === newValue) return
+        this.removeEntry(individual, datafield, oldValue)
+        this.addEntry(individual, datafield, newValue)
+    }
+    removeEntry(individual, datafield, value) {
+        this.store.removeQuad(nnExpand(individual), nnExpand(datafield), parseObject(value))
+    }
+    addIndividual(clazz) {
+        let count = this.store.match(null, nnExpand("rdf:type"), nnExpand(clazz)).size
+        const uri = expand(clazz).toLowerCase() + (count + 1)
+        this.addEntry(uri, "rdf:type", clazz)
+        return uri
+    }
     materialize() {}
     validate() {}
     export() {}
-    serialize() {}
+    async toTurtle() {
+        return await storeToTurtle(this.store)
+    }
 }
--- test/profile-manager.test.js
+++ test/profile-manager.test.js
@@ -1,11 +1,28 @@
 import { describe } from "mocha"
+import { strictEqual } from "node:assert"
+import { isomorphicTurtles } from "@foerderfunke/sem-ops-utils"
 
-describe.skip("profile manager tests", function () {
+describe("profile manager tests", function () {
     let profileManager
 
     before(async function () {
         profileManager = globalThis.profileManager
+        profileManager.newProfile()
     })
 
-    it("TODO", function () {})
+    it("test basic triple functions", async function () {
+        let profile = profileManager.profiles["profile1"]
+        profile.addIndividual("ff:Citizen")
+        profile.addEntry("ff:citizen1", "ff:hasName", "John")
+        profile.addEntry("ff:citizen1", "ff:hasAge", 30)
+        profile.changeEntry("ff:citizen1", "ff:hasAge", 30, 40)
+        profile.removeEntry("ff:citizen1", "ff:hasName", "John")
+        profile.addIndividual("ff:Citizen")
+        let actualTurtle = await profile.toTurtle()
+        let expectedTurtle = `
+            @prefix ff: <https://foerderfunke.org/default#>.
+            ff:citizen1 a ff:Citizen ; ff:hasAge 40 .
+            ff:citizen2 a ff:Citizen .`
+        strictEqual(isomorphicTurtles(actualTurtle, expectedTurtle), true, "The turtles are not the same")
+    })
 })
