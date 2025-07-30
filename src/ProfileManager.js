8d77e114cadca504f3bd271a465acbcfc1535333
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-07-30 16:17:10 +0200

    Add materializeAndValidate()

--- src/ProfileManager.js
+++ src/ProfileManager.js
@@ -1,4 +1,4 @@
-import { getRdf, buildValidatorFromDataset, datasetFromStore, datasetFromTurtles, expand, newStore, nnExpand, parseObject, storeFromTurtles, storeToTurtle } from "@foerderfunke/sem-ops-utils"
+import { getRdf, buildValidatorFromDataset, datasetFromStore, datasetFromTurtles, expand, newStore, nnExpand, parseObject, storeFromTurtles, storeToTurtle, sparqlConstruct, sparqlAsk, datasetToTurtle } from "@foerderfunke/sem-ops-utils"
 
 const ns = {
     ff: getRdf().namespace("https://foerderfunke.org/default#")
@@ -28,13 +28,13 @@ export class ProfileManager {
     }
     newProfile() {
         let id = this.generateProfileId()
-        this.profiles[id] = new Profile(id)
+        this.profiles[id] = new Profile(id, this)
         this.profiles[id].addEntry("ff:user", "rdf:type", "ff:Citizen")
         return id
     }
     importProfileTurtle(turtle) {
         let id = this.generateProfileId()
-        this.profiles[id] = new Profile(id)
+        this.profiles[id] = new Profile(id, this)
         this.profiles[id].importTurtle(turtle)
         return id
     }
@@ -44,10 +44,12 @@ export class ProfileManager {
 }
 
 class Profile {
-    constructor(id) {
+    constructor(id, profileManager) {
         this.id = id
+        this.profileManager = profileManager
         this.nickname = null
         this.store = newStore()
+        this.enrichedStore = null
     }
     addEntry(individual, datafield, value) {
         this.store.addQuad(nnExpand(individual), nnExpand(datafield), parseObject(value))
@@ -66,8 +68,38 @@ class Profile {
         this.addEntry(uri, "rdf:type", clazz)
         return uri
     }
-    materialize() {}
-    validate() {}
+    async materializeAndValidate() {
+        await this.materialize()
+        return await this.validate()
+    }
+    async materialize() {
+        this.enrichedStore = newStore()
+        for (let query of Object.values(this.profileManager.matQueries)) {
+            // do we need an exhausting approach instead until nothing is materialized anymore instead of a one-time for loop?
+            // also filling the upStore while also using it as source could create build-ups with side effects?
+            await sparqlConstruct(query, [this.store, this.profileManager.defStore], this.enrichedStore)
+        }
+    }
+    async validate() {
+        let query = `
+            PREFIX ff: <https://foerderfunke.org/default#>
+            ASK { ?user a ff:Citizen . }`
+        if (!await sparqlAsk(query, [this.enrichedStore])) {
+            return { conforms: false, report: "User profile does not contain an individual of class ff:Citizen" }
+        }
+        let ds = datasetFromStore(this.enrichedStore)
+        // plausibility validation
+        let pReport = await this.profileManager.datafieldsValidator.validate({ dataset: ds })
+        if (!pReport.conforms) {
+            return { conforms: false, report: await datasetToTurtle(pReport.dataset) }
+        }
+        // logical consistency validation
+        let lcReport = await this.profileManager.consistencyValidator.validate({ dataset: ds })
+        if (!lcReport.conforms) {
+            return { conforms: false, report: await datasetToTurtle(lcReport.dataset) }
+        }
+        return { conforms: true, dataset: ds }
+    }
     importTurtle(turtle) {
         this.store = storeFromTurtles([turtle])
     }
